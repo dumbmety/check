4281518cf2b7a072b495ba171061fe8edd29719f
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-11-04 00:51:25 +0330

    refactor: task card component using tailwindcss

--- frontend/views/tasks/TaskCard.tsx
+++ frontend/views/tasks/TaskCard.tsx
@@ -1,7 +1,6 @@
 import moment from "moment"
-import styled, { css } from "styled-components"
 
-import { theme } from "../../components/styles/ThemeStyles"
+import { classNames } from "../../utils/classNames"
 import Checkbox from "../../components/shared/Checkbox"
 import Icon from "../../components/shared/Icon"
 
@@ -20,33 +19,34 @@ interface ITaskCardProps {
   onChecked: (id: string) => void
 }
 
-interface IWrapperProps {
-  isChecked: boolean
-}
-
-interface IGroupProps {
-  isFull?: boolean
-}
-
 export default function TaskCard(props: ITaskCardProps) {
   return (
-    <Wrapper isChecked={props.checked}>
-      <Group>
+    <div
+      className={classNames(
+        "py-2 px-4 rounded space-x-4 flex items-center select-none transition duration-150 ease-in-out",
+        props.checked && "opacity-75"
+      )}
+    >
+      <div className="py-1">
         <Checkbox
           checked={props.checked}
           onClick={() => props.onChecked(props.id)}
         />
-      </Group>
-      <Group isFull>
-        <Title>{props.title}</Title>
-      </Group>
-      <Group>
-        <Info>
+      </div>
+      <div className="flex-1 flex items-center">
+        <h3
+          className={classNames(props.checked && "text-gray100 line-through")}
+        >
+          {props.title}
+        </h3>
+      </div>
+      <div className="py-1">
+        <div className="flex items-center mt-1 opacity-50 space-x-1 cursor-pointer">
           {props.date && (
-            <Date>
+            <div className="space-x-1 flex items-center text-sm mr-1">
               <Icon name="Calendar" width={20} height={20} />
               <span>{moment(props.date).format("ll")}</span>
-            </Date>
+            </div>
           )}
           {props.date?.length > 0 && (
             <Icon name="Note" width={20} height={20} />
@@ -54,70 +54,8 @@ export default function TaskCard(props: ITaskCardProps) {
           {props.file?.length > 0 && (
             <Icon name="Folder" width={20} height={20} />
           )}
-        </Info>
-      </Group>
-    </Wrapper>
+        </div>
+      </div>
+    </div>
   )
 }
-
-const Wrapper = styled.div<IWrapperProps>`
-  padding: 0.5rem 0.8rem;
-  border-radius: 5px;
-  gap: 1rem;
-  display: flex;
-  align-items: center;
-  user-select: none;
-  transition: all 0.3s ease-in-out;
-
-  ${props =>
-    props.isChecked &&
-    css`
-      opacity: 0.75;
-
-      h3 {
-        color: ${theme.colors.gray100};
-        text-decoration: line-through;
-      }
-    `}
-`
-
-const Group = styled.div<IGroupProps>`
-  padding: 0.25rem 0;
-
-  ${props =>
-    props.isFull &&
-    css`
-      flex: 1;
-      padding: 0;
-      display: flex;
-      align-items: center;
-    `}
-`
-
-const Title = styled.h3`
-  transition: all 0.3s ease-in-out;
-`
-
-const Info = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 0.25rem;
-  opacity: 0.5;
-  gap: 0.25rem;
-
-  * {
-    cursor: pointer;
-  }
-`
-
-const Date = styled.div`
-  gap: 0.25rem;
-  display: flex;
-  align-items: center;
-  font-size: 0.8rem;
-  margin-right: 0.25rem;
-
-  span {
-    line-height: 1.6;
-  }
-`
