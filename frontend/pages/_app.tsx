2d103f7848216ed9432bee358c0dfd3648fc58bb
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-11-04 00:39:10 +0330

    feat: add tailwindcss

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,16 +1,14 @@
 import "@fontsource/inter"
 import "simplebar/dist/simplebar.min.css"
+import "../styles/global.css"
 
 import type { AppProps } from "next/app"
 
-import GlobalStyles from "../components/styles/GlobalStyles"
 import MainLayout from "../components/layout/MainLayout"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyles />
-
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  mode: "jit",
+  darkMode: false,
+  purge: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./views/**/*.{ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        indigo100: "#8B96F4",
+        indigo200: "#818DF3",
+        indigo300: "#7885F2",
+        indigo400: "#6F7DF1",
+        indigo500: "#5C6CEF",
+        indigo600: "#4755c5",
+        indigo700: "#3c4bbe",
+
+        green100: "#B2E8A5",
+        green200: "#A5E082",
+        green300: "#8FD966",
+        green400: "#7ACD4F",
+        green500: "#5FB93C",
+
+        red100: "#F58A8A",
+        red200: "#F48080",
+        red300: "#F37777",
+        red400: "#F26E6E",
+        red500: "#F05B5B",
+
+        blue100: "#7AA8FF",
+        blue200: "#70A1FF",
+        blue300: "#669AFF",
+        blue400: "#5C93FF",
+        blue500: "#4886FF",
+
+        black100: "#3B3A41",
+        black200: "#37363B",
+        black300: "#313036",
+        black400: "#2C2B30",
+        black500: "#28272C",
+
+        gray100: "#929197",
+        gray200: "#8A8990",
+        gray300: "#85848B",
+        gray400: "#807F86",
+        gray500: "#75747B",
+
+        white100: "#EBEBEB",
+        white200: "#F0F0F0",
+        white300: "#F5F5F5",
+        white400: "#FAFAFA",
+        white500: "#FFFFFF",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
